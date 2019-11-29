d07043517970935eb788bb4cee0cd9b24e669672
Michael Lesniak <mail@mlesniak.com>
2019-11-29 19:48:18 +0100

    Example writer

--- main.go
+++ main.go
@@ -3,21 +3,54 @@ package main
 import (
 	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
 	"log"
-	"time"
 )
 
 func main() {
-	log.Println("Starting")
-	go listen("client-1")
-	go listen("client-2")
+	//log.Println("Starting")
+	//go listen("client-1")
+	//go listen("client-2")
+	//
+	//time.Sleep(1000000 * 1000 * 10)
+	//
+	//go listen("client-3 / new")
+	//
+	//for {
+	//	time.Sleep(1000 * 1000 * 3600)
+	//}
 
-	time.Sleep(1000000 * 1000 * 10)
-
-	go listen("client-3 / new")
-
-	for {
-		time.Sleep(1000 * 1000 * 3600)
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		panic(err)
 	}
+
+	defer p.Close()
+
+	// Delivery report handler for produced messages
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					log.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					log.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+
+	// Produce messages to topic (asynchronously)
+	topic := "topic4"
+	for _, word := range []string{"Welcome", "to", "the", "Confluent", "Kafka", "Golang", "client"} {
+		p.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          []byte(word),
+		}, nil)
+	}
+
+	// Wait for message deliveries before shutting down
+	left := p.Flush(15 * 1000)
+	log.Println("Left events:", left)
 }
 
 func listen(name string) {
