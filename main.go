ca8902e252d4c6b8c59dcd9206924dc7389a38a3
Michael Lesniak <mail@mlesniak.com>
2019-11-29 13:19:03 +0100

    Add reader

--- main.go
+++ main.go
@@ -6,15 +6,14 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, Kafka")
-
 	config := kafka.NewConfig()
 	client, err := kafka.NewClient([]string{":9092"}, config)
 	if err != nil {
 		panic(err)
 	}
 
-	producer(client)
+	// producer(client)
+	consumer(client)
 }
 
 func producer(client kafka.Client) {
@@ -24,6 +23,31 @@ func producer(client kafka.Client) {
 		Value: kafka.StringEncoder("Hello, + ${time.Now()}"),
 	}
 	producer.Input() <- &m
+	// TODO Read error channel to prevent blocking
 	
 	producer.Close()
+}
+
+func consumer(client kafka.Client) {
+	consumer, err := kafka.NewConsumerFromClient(client)
+	if err != nil {
+		panic(err)
+	}
+
+	topics, _ := consumer.Topics()
+	fmt.Println("--- Topics")
+	for _, value := range topics {
+		fmt.Println(value)
+	}
+
+	part, err := consumer.ConsumePartition("topic", 0, kafka.OffsetOldest)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("--- Messages")
+	messages := part.Messages()
+	for m := range messages {
+		fmt.Println(string(m.Value))
+	}
 }
