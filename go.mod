6e3597a502e40f36fd79c9b401a58d8c1906351b
Michael Lesniak <mail@mlesniak.com>
2019-11-29 19:41:39 +0100

    Using confluents kafka library which is easier to use

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/Shopify/sarama v1.24.1
 	golang.org/x/tools v0.0.0-20191127201027-ecd32218bd7f // indirect
+	gopkg.in/confluentinc/confluent-kafka-go.v1 v1.1.0
 )
--- main.go
+++ main.go
@@ -1,102 +1,44 @@
 package main
 
 import (
-	"context"
+	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
 	"log"
-
-	//"context"
-	"fmt"
-	kafka "github.com/Shopify/sarama"
+	"time"
 )
 
 func main() {
-	config := kafka.NewConfig()
-	config.Version = kafka.MaxVersion
-	client, err := kafka.NewClient([]string{":9092"}, config)
-	if err != nil {
-		panic(err)
-	}
+	log.Println("Starting")
+	go listen("client-1")
+	go listen("client-2")
 
-	// producer(client)
-	//consumer(client)
-	consumerGroup(client)
-}
+	time.Sleep(1000000 * 1000 * 10)
 
-func producer(client kafka.Client) {
-	producer, _ := kafka.NewAsyncProducerFromClient(client)
-	m := kafka.ProducerMessage{
-		Topic: "topic",
-		Value: kafka.StringEncoder("Hello, + ${time.Now()}"),
-	}
-	producer.Input() <- &m
-	// TODO Read error channel to prevent blocking
+	go listen("client-3 / new")
 
-	producer.Close()
-}
-
-func consumerGroup(client kafka.Client) {
-	v := client.Config().Version
-	fmt.Println(v)
-
-	group, err := kafka.NewConsumerGroupFromClient("readers", client)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("--- Messages")
-	ctx := context.Background()
 	for {
-		// Simulate two consumer.
-		go func() {
-			fmt.Println("Starting parallel consumer")
-			group.Consume(ctx, []string{"topic4"}, groupHandler{"client 0"})
-		}()
-		group.Consume(ctx, []string{"topic4"}, groupHandler{"client 1"})
+		time.Sleep(1000 * 1000 * 3600)
 	}
 }
 
-func consumer(client kafka.Client) {
-	consumer, err := kafka.NewConsumerFromClient(client)
+func listen(name string) {
+	log.Println("Starting consumer", name)
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": ":9092",
+		"group.id":          "group",
+		//"auto.offset.reset": "earliest",
+	})
 	if err != nil {
 		panic(err)
 	}
-
-	topics, _ := consumer.Topics()
-	fmt.Println("--- Topics")
-	for _, value := range topics {
-		fmt.Println(value)
-	}
-
-	part, err := consumer.ConsumePartition("topic4", 0, kafka.OffsetOldest)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("--- Messages")
-	messages := part.Messages()
-	for m := range messages {
-		fmt.Println(string(m.Value))
-	}
-}
-
-type groupHandler struct {
-	Name string
-}
-
-func (g groupHandler) Setup(session kafka.ConsumerGroupSession) error {
-	fmt.Println("Started", g.Name)
-	fmt.Println("Claims:", session.Claims())
-	return nil
-}
-func (groupHandler) Cleanup(_ kafka.ConsumerGroupSession) error { return nil }
-func (h groupHandler) ConsumeClaim(sess kafka.ConsumerGroupSession, claim kafka.ConsumerGroupClaim) error {
-	for msg := range claim.Messages() {
-		fmt.Printf("Message topic:%q partition:%d offset:%d value:%s\n", msg.Topic, msg.Partition, msg.Offset, string(msg.Value))
-		sess.MarkMessage(msg, "")
-		if sess.Context().Err() != nil {
-			log.Println("Context switch")
-			return nil
+	c.SubscribeTopics([]string{"topic4"}, nil)
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			log.Printf("%s // Message on %s: %s\n", name, msg.TopicPartition, string(msg.Value))
+		} else {
+			// The client will automatically try to recover from all errors.
+			log.Printf("Consumer error: %v (%v)\n", err, msg)
 		}
 	}
-	return nil
+	c.Close()
 }
